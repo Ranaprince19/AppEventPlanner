d4961c1ae6b001bf8de7af50dbdba82882045798
Prince Kumar <rajputprince3620@gmail.com>
2025-11-06 22:51:43 +0530

    Buisness Screen

---
+++ EventPlannerApp/BuiisnessViewController.swift
@@ -0,0 +1,42 @@
+//
+//  BuiisnessViewController.swift
+//  EventPlannerApp
+//
+//  Created by Prince Rana on 06/11/25.
+//
+
+import UIKit
+
+class BuiisnessViewController: UIViewController {
+
+    @IBAction func nextButtons(_ sender: Any) {
+    }
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var aboutusTextField: UITextField!
+    @IBOutlet weak var aboutusLabel: UILabel!
+    @IBOutlet weak var websiteTextField: UITextField!
+    @IBOutlet weak var websiteLabel: UILabel!
+    @IBOutlet weak var buisnessAddressTextField: UITextField!
+    @IBOutlet weak var buisnessAddressLabel: UILabel!
+    @IBOutlet weak var businessNameTextField: UITextField!
+    @IBOutlet weak var businessNameLabel: UILabel!
+    @IBOutlet weak var businessSubtitle: UILabel!
+    @IBOutlet weak var businessTitle: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
